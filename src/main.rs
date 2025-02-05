e656681ed3129f296521832da32d6db158155b92
choigunwoo <gunwoo8873@outlook.kr>
2025-02-05 15:08:22 +0900

    Fix : Discord bot main run file

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,51 @@
-use std::env;
-use dotenv::dotenv;
+// use std::env;
+// use dotenv::dotenv;
 
-use serenity::async_trait;
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
+// use serenity::async_trait;
+// use serenity::model::channel::Message;
+// use serenity::model::gateway::Ready;
+// use serenity::prelude::*;
 
 
-struct Handler;
+// struct Handler;
 
-#[async_trait]
-impl EventHandler for Handler {
-  async fn message(&self, ctx: Context, msg: Message) {
-    if msg.content == "ping" {
-      if let Err(why) = msg.channel_id.say(&ctx.http, "pong").await {
-        println!("Error sending message: {:?}", why);
-      }
-    }
-  }
+// #[async_trait]
+// impl EventHandler for Handler {
+//   async fn message(&self, ctx: Context, msg: Message) {
+//     if msg.content == "ping" {
+//       if let Err(why) = msg.channel_id.say(&ctx.http, "pong").await {
+//         println!("Error sending message: {:?}", why);
+//       }
+//     }
+//   }
 
-  async fn ready(&self, _: Context, ready: Ready) {
-    println!("{} is connected!", ready.user.name);
-  }
-}
+//   async fn ready(&self, _: Context, ready: Ready) {
+//     println!("{} is connected!", ready.user.name);
+//   }
+// }
 
-#[tokio::main]
-async fn main() {
-  dotenv().ok();
-  let token = env::var("DISCORD_BOT_TOKEN").expect("Expected a token in the environment");
-  println!("Token: {}", token);
+// #[tokio::main]
+// async fn main() {
+//   dotenv().ok();
+//   let token = env::var("DISCORD_BOT_TOKEN").expect("Expected a token in the environment");
+//   println!("Token: {}", token);
   
-  let intents = GatewayIntents::GUILD_MESSAGES
-  | GatewayIntents::DIRECT_MESSAGES
-  | GatewayIntents::GUILD_MESSAGE_REACTIONS;
+//   let intents = GatewayIntents::GUILD_MESSAGES
+//   | GatewayIntents::DIRECT_MESSAGES
+//   | GatewayIntents::GUILD_MESSAGE_REACTIONS;
 
-  let mut client = Client::builder(&token, intents)
-  .event_handler(Handler)
-  .await
-  .expect("Err creating client");
+//   let mut client = Client::builder(&token, intents)
+//   .event_handler(Handler)
+//   .await
+//   .expect("Err creating client");
 
-  if let Err(why) = client.start().await {
-    println!("Client error: {:?}", why);
-  }
+//   if let Err(why) = client.start().await {
+//     println!("Client error: {:?}", why);
+//   }
+// }
+
+use DiscordBot::bot::client::discord_run;
+
+fn main() {
+  discord_run();
 }
